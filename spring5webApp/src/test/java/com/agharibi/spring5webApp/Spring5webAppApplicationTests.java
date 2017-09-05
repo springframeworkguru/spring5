b0420259c15945e3cee1cce4a6ad426add10246f
Armen G <gharibi.armen@yahoo.com>
2017-09-04 19:39:31 -0700

    spring5 webapp initializer

---
+++ spring5webApp/src/test/java/com/agharibi/spring5webApp/Spring5webAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agharibi.spring5webApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5webAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
